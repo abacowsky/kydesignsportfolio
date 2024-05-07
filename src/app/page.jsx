a331d1e7fc1326f5f1f6df79cf61d488075f5e20
abacowsky <stabbatkennels@gmail.com>
2024-05-06 18:27:04 -0700

    adding new picture

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,37 +5,54 @@ import { motion } from "framer-motion";
 
 const Homepage = () => {
   return (
-    <motion.div className="h-full" 
-    initial={{y:"-200vh"}} 
-    animate={{y:"0%"}} 
-    transition={{ duration: 1 }}
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
     >
-  <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px48">
-    {/*IMAGE CONTAINER*/}
-    <div className="h-full w-full lg:h-full lg:w-1/2 relative">
-      <Image src="/caribou.png" alt="" fill className="object-contain"/>
-    </div>
-      {/*TEXT CONTAINER*/}
-      <div className='h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center'>
-      {/*TITLE*/}
-      <h1 className="text-4xl md:text-6xl font-bold">Let Us Design Together</h1>
-      {/*DESCRIPTION*/}
-      <p className="md:text-xl">The heart and soul of my design are honesty, passion, and poise. Honesty in design is like a signature. 
-        It is what makes a design authentically yours. Passion is the fuel that keeps us burning through late night 
-        projects, endless revisions, and onto the next wonderful creation. And Poise? It&lsquo;s the grace with which we 
-        handle every challenge, having empathy during bad days, and delivering stunning visuals. These aren&lsquo;t just values, 
-        they are the essence of who I am as a designer and as an individual. These are what drive me to create designs that 
-        speak, resonate, and inspire. Staying true, keeping my passion, and designing with elegance.</p>
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px48">
+        {/* IMAGE CONTAINER */}
+        <div className="h-full w-full lg:h-full lg:w-1/2 relative">
+          <Image src="/hero.png" alt="Hero Image" layout="fill" objectFit="cover" />
+        </div>
 
-        {/*BUTTONS*/}
-        <div className="w-full flex gap-4">
-          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">My Work</button>
-          <button className="p-4 rounded-lg ring-1 ring-black">Contact Me</button>
+        {/* ANIMATED LOGO */}
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 1 }}
+            animate={{ rotate: 360, opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, duration: 2, loop: Infinity }}
+            style={{ position: "absolute", bottom: 0, left: 0 }}
+          >
+            <Image src="/logopink.png" alt="Logo" width={100} height={100} />
+          </motion.div>
+        </div>
+
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          {/* TITLE */}
+          <h1 className="text-4xl md:text-6xl font-bold">Let Us Design Together</h1>
+          {/* DESCRIPTION */}
+          <p className="md:text-xl">
+            The heart and soul of my design are honesty, passion, and poise. Honesty in design is like a signature. It is
+            what makes a design authentically yours. Passion is the fuel that keeps us burning through late-night projects,
+            endless revisions, and onto the next wonderful creation. And Poise? It&lsquo;s the grace with which we handle
+            every challenge, having empathy during bad days, and delivering stunning visuals. These aren&lsquo;t just
+            values, they are the essence of who I am as a designer and as an individual. These are what drive me to create
+            designs that speak, resonate, and inspire. Staying true, keeping my passion, and designing with elegance.
+          </p>
+
+          {/* BUTTONS */}
+          <div className="w-full flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">My Work</button>
+            <button className="p-4 rounded-lg ring-1 ring-black">Contact Me</button>
           </div>
+        </div>
       </div>
-  </div>
-  </motion.div>
+    </motion.div>
   );
 };
 
 export default Homepage;
+
